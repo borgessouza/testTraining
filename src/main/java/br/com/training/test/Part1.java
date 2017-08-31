1e13af4b7a4923c4e1c22461d75a364ab9913535
cassiano.souza <cassiano.souza@tivit.com.br>
2017-08-31 17:56:57 -0300

    Initial processs

---
+++ src/main/java/br/com/training/test/Part1.java
@@ -0,0 +1,18 @@
+package br.com.training.test;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Part1 {
+
+	
+	
+	public List<?> vazio() {
+		return Collections.emptyList();
+	}
+
+	
+	
+	
+	
+}
